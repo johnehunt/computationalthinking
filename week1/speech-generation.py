8de92a666a5bb594463d436215a3414c19f53f89
John Hunt <john@matraining.com>
2019-09-17 11:01:42 +0100

    Added print statements

--- week1/speech-generation.py
+++ week1/speech-generation.py
@@ -1,10 +1,14 @@
 import pyttsx3
 
+print('Initializing System')
 # Initialize the speech generation library
 engine = pyttsx3.init()
 
+print('Set up text to say')
 # Provide some text to 'say'
 engine.say('Good morning.')
 
 # Runs the engine and waits until the text has been 'said'
 engine.runAndWait()
+
+print('Done')
