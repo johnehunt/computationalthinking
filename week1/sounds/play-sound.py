93ba62502360328aad965f0148fb6e3b313aeb28
John Hunt <john@matraining.com>
2019-09-17 12:12:10 +0100

    Added return to allow the tune to play until the user wants to exit

--- week1/sounds/play-sound.py
+++ week1/sounds/play-sound.py
@@ -2,12 +2,14 @@ import pygame
 
 print('Initializing system')
 # Initialise play game and the sound system
-pygame.init()
 pygame.mixer.init()
 
 print('Play sound')
 # Load the sound file and play it once
 pygame.mixer.music.load('bensound-epic.mp3')
 pygame.mixer.music.play() # -1 to loop
+
+# Allow the tune to play
+input('Press return to finish')
 
 print('Done')
