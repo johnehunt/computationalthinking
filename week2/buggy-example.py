f3c0ead2ce88e0bcab7a96435a9a04ecfed6d4f1
John Hunt <john@matraining.com>
2019-09-30 11:53:46 +0100

    Commented out code examples to avoid issues when imported into an IDE

--- week2/buggy-example.py
+++ week2/buggy-example.py
@@ -1,10 +1,11 @@
-# Can you deb ug this file
+# Can you debug this file
+# Comment the following lines and resolve the issues
 
-print('hello', name)
-
-count = count + 1
-print(count)
-
-print(john)
-
-print('Goodbye)
+# print('hello', name)
+#
+# count = count + 1
+# print(count)
+#
+# print(john)
+#
+# print('Goodbye)
