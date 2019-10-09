0b966d0c5ad22f287259515a19f36b5aca02416a
John Hunt <john@matraining.com>
2019-10-09 08:20:37 +0100

    SOme exam ples of using a for loop with collections of data rather than ranges

---
+++ week4/forWithLists.py
@@ -0,0 +1,17 @@
+# This file illustrates examples of using for loops with data containerts / collections
+
+my_list = [1, 2, 3, 4]
+for item in my_list:
+    print(item)
+
+print('-' * 10)
+
+my_set = {'a', 'b', 'c'}
+for item in my_set:
+    print(item)
+
+print('-' * 10)
+
+my_tuple = (1, 2, 3, 4)
+for item in my_tuple:
+    print(item)
