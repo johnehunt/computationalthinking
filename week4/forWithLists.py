f34bef515ed96619e825ffcba0943650f06c4606
John Hunt <john@matraining.com>
2019-10-10 10:24:54 +0100

    Added more comments to the examples

--- week4/forWithLists.py
+++ week4/forWithLists.py
@@ -1,17 +1,20 @@
 # This file illustrates examples of using for loops with data containerts / collections
 
+# Loop through a list of values
 my_list = [1, 2, 3, 4]
 for item in my_list:
     print(item)
 
 print('-' * 10)
 
+# Loop through a set of values
 my_set = {'a', 'b', 'c'}
 for item in my_set:
     print(item)
 
 print('-' * 10)
 
+# Loop through a fixed / immutable tuple of values
 my_tuple = (1, 2, 3, 4)
 for item in my_tuple:
     print(item)
--- week4/forsample.py
+++ week4/forsample.py
@@ -16,13 +16,7 @@ print('Done')
 
 print('-' * 25)
 
-# Now use an 'anonymous' loop variable
-for _ in range(0,10):
-    print('.', end='')
-print()
-
-print('-' * 25)
-
+# This illustrates the use of a break statement
 print('Only print code if all iterations completed')
 num = int(input('Enter a number to check for: '))
 for i in range(0, 6):
@@ -33,23 +27,11 @@ print('Done')
 
 print('-' * 25)
 
+# This illustrates the use of a continue statement
 for i in range(0, 10):
     print(i, ' ', end='')
     if i % 2 == 1:
         continue
     print('hey its an even number')
     print('we love even numbers')
-print('Done')
-
-print('-' * 25)
-
-print('Only print code if all iterations completed')
-num = int(input('Enter a number to check for: '))
-for i in range(0, 6):
-    if i == num:
-        break
-    print(i, ' ', end='')
-else:
-    print()
-    print('All iterations successful')
 print('Done')
