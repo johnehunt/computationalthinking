9754930bca42fbaef6cee137c76417f90d96245c
John Hunt <john@matraining.com>
2019-10-15 10:28:51 +0100

    Simplified example and added a constant for the max number of guesses

--- week4/number_guess_game.py
+++ week4/number_guess_game.py
@@ -1,39 +1,52 @@
 import random
 
-print('Welcome to the number guess game')
+# Initialize the maximum number of guess
+MAX_NUMBER_OF_GUESSES = 4
 
-# Initialise the number to be guessed
-number_to_guess = random.randint(1,10)
+# Print the Welcome Banner
+print("=" * 25)
+print('Welcome to the Number Guess Game')
+print("=" * 25)
 
-# Initialise the number of tries the player has made
-count_number_of_tries = 1
+# Set up the game playing loop
+game_over = False
+while not game_over:
 
-# Obtain their initial guess
-guess = int(input('Please guess a number between 1 and 10: '))
-while number_to_guess != guess:
-    print('Sorry wrong number')
+    # Initialise the number to be guessed
+    number_to_guess = random.randint(1, 10)
 
-    # Check to see they have not exceeded the maximum
-    # number of attempts if so break out of loop otherwise
-    # give the user come feedback
-    if count_number_of_tries == 4:
-        break
-    elif guess < number_to_guess:
-        print('Your guess was lower than the number')
-    else:
-        print('Your guess was higher than the number')
+    # Initialise the number of tries the player has made
+    count_number_of_tries = 0
 
-    # Obtain their next guess and increment number of attempts
-    guess = int(input('Please guess again: '))
-    count_number_of_tries += 1
+    # Set up flag to indicate if one game is over
+    finished_current_game = False
 
-# Check to see if they did guess the correct number
-if number_to_guess == guess:
-    print('Well done you won!')
-    print('You took', count_number_of_tries , 'goes to complete the game')
-else:
-    print("Sorry - you loose")
-    print('The number you needed to guess was',
-          number_to_guess)
+    # Obtain their initial guess and loop to see if they guessed correctly
+    while not finished_current_game:
+        guess = int(input('Please guess a number between 1 and 10: '))
+        count_number_of_tries += 1
+
+        # Check to see they have not exceeded the maximum
+        # number of attempts if so break out of loop otherwise
+        # give the user come feedback
+        if count_number_of_tries == MAX_NUMBER_OF_GUESSES:
+            print("Sorry - you loose")
+            print('The number you needed to guess was',
+                  number_to_guess)
+            finished_current_game = True
+        elif guess < number_to_guess:
+            print('Your guess was lower than the number')
+        elif guess > number_to_guess:
+            print('Your guess was higher than the number')
+        else:
+            print('Well done you won!')
+            print('You took', count_number_of_tries, 'goes to complete the game')
+            finished_current_game = True
+
+    # Check to see if the user wants to play another game
+    response = input('Do you want to play again? (y/n): ')
+    response = response.lower()
+    if response == 'n':
+        game_over = True
 
 print('Game Over')
