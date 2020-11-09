2f83c48ce6180542dec4afc3122114070ee6025a
John Hunt <john.hunt10@gmail.com>
2020-11-09 11:45:12 +0000

    Updated to start with latest version

--- week4/module_marks.py
+++ week4/module_marks.py
@@ -3,14 +3,14 @@ student_name = input('Please enter your name: ')
 
 # Ask the user for the students marks
 coursework_mark = int(input('Coursework mark: '))
-exam_mark = int(input('Combined in-class test and exam: '))
+first_exam_mark = int(input('first exam mark: '))
+second_exam_mark = int(input('second exam mark: '))
 
-# Check for a pass or fail
-if coursework_mark < 35:
-    print('Fail')
-elif exam_mark < 35:
-    print('Fail')
-elif (coursework_mark + exam_mark) / 2 < 40:
-    print('Fail')
-else:
-    print('You passed')
+coursework_mark = coursework_mark * 0.6
+first_exam_mark = first_exam_mark * 0.15
+second_exam_mark = second_exam_mark * 0.25
+
+module_mark = coursework_mark + first_exam_mark + second_exam_mark
+
+print("\nDear", student_name)
+print("Your total mark for this module is", str(int(module_mark))+'%')
--- week4/module_marks2.py
+++ week4/module_marks2.py
@@ -1,28 +1,12 @@
-# Version that checks for valid input
-
 # Prompt for the the student's name
 student_name = input('Please enter your name: ')
 
 # Ask the user for the students marks
-# Initially input as a string
-coursework_mark = input('Coursework mark: ')
-if coursework_mark.isdigit():
-    coursework_mark = int(coursework_mark)
-else:
-    print('Your course mark should be a positive integer')
-    coursework_mark = -1
-
-exam_mark = input('Combined in-class test and exam: ')
-if exam_mark.isdigit():
-    exam_mark = int(exam_mark)
-else:
-    print('Your combined in-class test and exam should be a positive integer')
-    exam_mark = -1
+coursework_mark = int(input('Coursework mark: '))
+exam_mark = int(input('Combined in-class test and exam: '))
 
 # Check for a pass or fail
-if coursework_mark == -1 or exam_mark == -1:
-    print('Invalid input')
-elif coursework_mark < 35:
+if coursework_mark < 35:
     print('Fail')
 elif exam_mark < 35:
     print('Fail')
