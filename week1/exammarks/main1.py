1b529f176bc7ed4357f9141500ea6c1374191a57
John Hunt <john@matraining.com>
2020-11-10 10:18:29 +0000

    Re ordered

--- week1/exammarks/main1.py
+++ week1/exammarks/main1.py
@@ -3,13 +3,13 @@
 component_a_mark = int(input('Please enter your mark for Component A: '))
 coursework_mark = int(input('Please enter your mark for Component B: '))
 
-module_mark = int(input('Please enter your mark for the module: '))
-
 print('Calculating your result')
 
-if coursework_mark < 35:
-    print('You have not passed the module')
-elif component_a_mark < 35:
+# Calculating mark by adding the marks together and dividing by 2
+module_mark = (component_a_mark + coursework_mark) / 2
+print('Your module mark is', module_mark)
+
+if coursework_mark < 35 or component_a_mark < 35:
     print('You have not passed the module')
 elif module_mark < 40:
     print('You have failed the module')
--- week1/speech/speech-generation.py
+++ week1/speech/speech-generation.py
@@ -6,7 +6,7 @@ engine = pyttsx3.init()
 
 print('Set up text to say')
 # Provide some text to 'say'
-engine.say('Good morning.')
+engine.say('Good afternoon.')
 
 # Runs the engine and waits until the text has been 'said'
 engine.runAndWait()
