f2944fd858c1a8498da67fbcc8c55a69205ccfbe
John Hunt <john@matraining.com>
2020-11-24 11:56:07 +0000

    Added further comments

--- week6/files1.py
+++ week6/files1.py
@@ -1,6 +1,11 @@
+# Open the file
 file = open('myfile.txt', 'r')
+
+# Access some properties of the file
 print('file.name:', file.name)
 print('file.closed:', file.closed)
 print('file.mode:', file.mode)
+
+# Close the link to the file
 file.close()
 print('file.closed now:', file.closed)
