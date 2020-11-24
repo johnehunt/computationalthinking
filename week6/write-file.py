0f3694b7977be5e3015b5a1ad64a50d0d8827842
John Hunt <john@matraining.com>
2020-11-24 11:58:37 +0000

    Added further comments

--- week6/write-file.py
+++ week6/write-file.py
@@ -1,7 +1,11 @@
 print('Writing file')
 f = open('my-new-file.txt', 'w')
-f.write('Hello from Python!!\n')
+
+# \n means newline
+f.write('Hello from Python!!' + '\n')
 f.write('Working with files is easy...\n')
 f.write('It is cool ...\n')
-f.write(str(34))  # Note write takes a string
+
+f.write(str(42))  # Note write takes a string
+f.write('\n')
 f.close()
