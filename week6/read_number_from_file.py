b21b4b16c360323639a15c121823ceebf8935085
John Hunt <john@matraining.com>
2020-11-24 10:37:20 +0000

    Provided exmaple of reading a number from a file

--- week6/read_number_from_file.py
+++ week6/read_number_from_file.py
@@ -1,7 +1,7 @@
 # Example illustratign reading numeric data from a file
 
 data = None
-with open('data.txt', 'r') as f:
+with open('info.dat', 'r') as f:
     data_as_string = f.readline()
     if data_as_string.isdigit():
         data = int(data_as_string)
