0342a2ace06cad9acfe8ec2a82b4f003a86e1458
John Hunt <john@matraining.com>
2020-11-24 11:56:22 +0000

    Changed layout

--- week6/files2.py
+++ week6/files2.py
@@ -1,9 +1,12 @@
 file = open('myfile.txt', 'r')
 
-# Returns the contents of the file as a list of strings
+# Returns the contents of the file as a
+# list of strings
 lines = file.readlines()
+
 for line in lines:
     print(line, end='')
 
+print()
 # Remember to always close the file
 file.close()
