4443eb931160f7876ac5c23bdb32ac14444da1a5
John Hunt <john@matraining.com>
2020-11-24 11:57:35 +0000

    Simplified example

--- week6/paths-app.py
+++ week6/paths-app.py
@@ -1,31 +1,18 @@
 from pathlib import Path
 
 print('Create Path object for current directory')
-p = Path('my_file.txt')
+p = Path('info.dat')
 print('p:', p)
 print('p.exists():', p.exists())
 print('p.is_dir():', p.is_dir())
 print('p.is_file():', p.is_file())
 print('p.absolute():', p.absolute())
 
-p = Path.cwd()
-print('Set up new directory')
-newdir = p / 'test'
-print('Check to see if newdir exists')
-print('newdir.exists():', newdir.exists())
-print('Create new dir')
-newdir.mkdir()
-print('newdir.exists():', newdir.exists())
+if p.exists():
+    print('yes the file exists')
+    for l in open(str(p.absolute())):
+        print(l)
 
-dir = Path('./test')
-print('Create new file')
-newfile = dir / 'text.txt'
-print('Write some text to file')
-newfile.write_text('Hello Python World!')
-print('Read the text back again')
-print(newfile.read_text())
 
-print('Remove the file')
-newfile.unlink()
-print('Remove the directory')
-newdir.rmdir()
+home = str(Path.home())
+print(home)
